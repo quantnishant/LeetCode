fc80ce7599d515ae4ab322af9f71500095cdf945
Nishant Kumar <98934625+quantnishant@users.noreply.github.com>
2025-01-19 12:57:39 +0530

    Time: 32 ms (87.81%), Space: 17.9 MB (9.96%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+class Solution:
+    def reverse(self, x: int) -> int:
         if -2**31 > x or x > 2**31 - 1:
             return 0
         else:
