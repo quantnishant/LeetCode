534f10b9ec2f250e20d7c5b59009344eadadf7f5
Nishant Kumar <98934625+quantnishant@users.noreply.github.com>
2025-03-15 17:06:37 +0530

    Time: 11 ms (29.84%), Space: 19.3 MB (52.2%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         freq_table = {}
-        for i in range(len(nums)):
-            if nums[i] in freq_table:
-                freq_table[nums[i]] += 1
-            else:
-                freq_table[nums[i]] = 1
-            if freq_table[nums[i]] > len(nums) // 2:
-                return nums[i]
+        threshold = len(nums) // 2 
+        
+        for num in nums:
+            freq_table[num] = freq_table.get(num, 0) + 1
+            if freq_table[num] > threshold:
+                return num
