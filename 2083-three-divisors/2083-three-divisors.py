befc793e021fb7334c0a088c318e2900c85ae698
Nishant Kumar <98934625+quantnishant@users.noreply.github.com>
2025-06-08 15:12:08 +0530

    Time: 0 ms (100%), Space: 17.7 MB (61.02%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.py
+++ 2083-three-divisors/2083-three-divisors.py
@@ -1,7 +1,9 @@
 class Solution:
     def isThree(self, n: int) -> bool:
         divisors_count = 0
-        for i in range(1,n+1):
-            if n % i == 0:
+        for i in range(1, int(math.sqrt(n)+1)):
+            if n % i == 0 and i != n//i:
+                divisors_count += 2
+            elif n % i == 0 and i == n//i:
                 divisors_count += 1
         return divisors_count == 3
