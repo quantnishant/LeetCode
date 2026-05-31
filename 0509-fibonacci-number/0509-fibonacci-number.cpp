3343706cedc90c5a55337b8437ed0d19f370df51
Nishant Kumar <98934625+quantnishant@users.noreply.github.com>
2026-05-31 16:45:02 +0530

    Time: 2 ms (76.88%), Space: 7.8 MB (38.61%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,13 +3,13 @@ public:
     int fib(int n) {
         int a = 0;
         int b = 1;
-        int c = 1;
-        for(int i = 0; i < n; i++){
-            c = a + b;
+
+        for (int i = 0; i < n; i++) {
+            int c = a + b;
             a = b;
             b = c;
         }
-    return a;
-    }
 
+        return a;
+    }
 };
